8ce59cb86ad43915969b6dcc276c5f1f8f950ed6
Jann Warnecke <45203588+padit1337@users.noreply.github.com>
2024-04-08 21:07:35 +0900

    Add files via upload

---
+++ dalle3_api.py
@@ -0,0 +1,42 @@
+import requests
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve API key from environment variables
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize OpenAI client with your API key
+client = OpenAI(api_key=api_key)
+
+# Number of times to generate an image
+i = 82
+
+for iteration in range(i):
+    # Generate an image
+    response = client.images.generate(
+      model="dall-e-3",
+      prompt="photorealistic whole, uncut, watermelon in a watermelon field",
+      size="1024x1024",
+      quality="standard",
+      n=1,
+    )
+
+    # Extract image URL from the response
+    image_url = response.data[0].url
+
+    # Use requests to download the image
+    response = requests.get(image_url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Save the image to a file with a unique name
+        filename = f"dalle3_watermelon{iteration+39}.png"
+        with open(filename, "wb") as file:
+            file.write(response.content)
+        print(f"Image successfully downloaded and saved as '{filename}'")
+    else:
+        print("Failed to download the image.")
